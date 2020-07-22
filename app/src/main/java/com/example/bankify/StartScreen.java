195137e69ceb783c286643286beb9ef1a43eb9d9
BuildTools <unconfigured@null.spigotmc.org>
2020-07-22 19:57:15 +0100

    StartScreen Design Changes

--- app/src/main/java/com/example/bankify/StartScreen.java
+++ app/src/main/java/com/example/bankify/StartScreen.java
@@ -6,34 +6,46 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.airbnb.lottie.LottieAnimationView;
 
 public class StartScreen extends AppCompatActivity {
 
     Context context = this;
+    private TextView bankifyTitle, getBankifyDesc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_start_screen);
 
         Button getStartedBtn = findViewById(R.id.getStarted);
         getStartedBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(context, ScrollingActivity.class);
+                Intent intent = new Intent(context, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
         });
 
+        bankifyTitle = findViewById(R.id.bankifyTitle);
+        getBankifyDesc = findViewById(R.id.bankifyDescription);
+
         Animation BottomToTop = AnimationUtils.loadAnimation(context, R.anim.bottom_to_top);
+        Animation TopToBottom = AnimationUtils.loadAnimation(context, R.anim.top_to_bottom);
 
         getStartedBtn.setAnimation(BottomToTop);
+        bankifyTitle.setAnimation(TopToBottom);
+        getBankifyDesc.setAnimation(TopToBottom);
 
     }
 }
